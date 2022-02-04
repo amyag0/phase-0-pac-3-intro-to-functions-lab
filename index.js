6d4fa58877ed8513a55f43b1901cae80b348de5c
Amy <amyago19@gmail.com>
2022-02-03 20:53:48 -0500

    commit tree 2ccc741c9fb1b8a36dc5a61a67ad0cf07fd1f682

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+    return string.toUpperCase();
+  }//shout
+
+function whisper(string) {
+    return string.toLowerCase();
+  }//whisper
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+  }
+
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string=="Let\'s have dinner together!"){
+       return console.log("I would love to!");
+    }
+
+    else if(string==toUpperCase) {
+        return console.log("YES INDEED!");
+    } 
+    
+    else if (string==toLowerCase){
+        return console.log("I can't hear you!");
+    }
+
+    }
+
+  
